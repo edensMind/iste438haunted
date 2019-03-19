d3dececb8c87f932eace1db66cf96878e08075a0
Malcolm Jones <malcolmljones@me.com>
2019-03-18 22:29:41 -0400

    Added 404 short circuit and proper headers

--- queries/getImage.php
+++ queries/getImage.php
@@ -1,14 +1,18 @@
 <?php
 
-// $record = $_POST["record"];
+require '../vendor/autoload.php';
 
-$record = "4501";
+$record = $_GET["record"];
 
-
-// In practice, $fileId denotes the _id of an existing file in GridFS
-$fileId = new MongoDB\BSON\ObjectId;
 $bucket = (new MongoDB\Client)->haunted->selectGridFSBucket();
-$data = $bucket->openDownloadStream($fileId);
+$file = $bucket->findOne(["filename" => $record]);
+if (is_null($file)) {
+  http_response_code(404);
+  die();
+}
+header("Content-Type: image/jpeg");
+header("Content-Length: " . $file['length']);
+$data = $bucket->openDownloadStream($file['_id']);
 $out = fopen('php://output', 'w');
 stream_copy_to_stream($data, $out);
 
